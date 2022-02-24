ceaa63c9c914460c2adfc2e1d30ed9ecec8c83e1
aria ortko <megymikei@gmail.com>
2022-02-24 22:44:02 +0300

    all tests are ok

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let ans = '';
+
+    let words = expr.split('**********');
+    let letter = '';
+
+    for (let i = 0; i < words.length; i++) {
+        let word = '';
+        for (let j = 0; j < words[i].length; j += 10) {
+            letter = '';
+            for (let cur = j; cur - j < 10; cur += 2) { 
+                if (words[i][cur] == '1') {
+                    if (words[i][cur + 1] == '1')
+                        letter += '-';
+                    else
+                        letter += '.';
+                }
+            }
+            word += MORSE_TABLE[letter];
+        }
+        ans += word + ' ';
+    }
+
+    return ans.trim();
 }
 
 module.exports = {
